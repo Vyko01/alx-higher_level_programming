38a0e89d5a25eaa83782789886141d9d9413c3bc
Vyko01 <allucius.nkuna@gmail.com>
2024-01-24 05:55:29 -0800

    jan

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,22 +1,9 @@
+#!/usr/bin/python3
+
+
 def safe_print_integer(value):
-    """
-    Prints an integer with "{:d}".format().
-
-    Prototype:
-    def safe_print_integer(value):
-
-    value can be any type (integer, string, etc.)
-    The integer should be printed followed by a new line
-    Returns True if value has been correctly printed (it means the value is an integer)
-    Otherwise, returns False
-
-    You have to use try: / except:
-    You have to use "{:d}".format() to print as integer
-    You are not allowed to import any module
-    You are not allowed to use type()
-    """
     try:
         print("{:d}".format(value))
-        return True
-    except ValueError:
-        return False
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
